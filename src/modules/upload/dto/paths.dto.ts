e65717705924f27779b084fe34c9be7d971cf71a
suk-6 <me@suk.kr>
2024-11-26 00:52:25 +0900

    feat: add base URL

--- src/modules/upload/dto/paths.dto.ts
+++ src/modules/upload/dto/paths.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class PathsDTO {
+  @ApiProperty({ description: "S3 Public URL" })
+  url: string;
+
   @ApiProperty({ description: "S3 Paths" })
   paths: string[];
 }
--- src/modules/upload/upload.service.ts
+++ src/modules/upload/upload.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 import { S3Service } from "src/common/modules/s3/s3.service";
 
 @Injectable()
 export class UploadService {
-  constructor(private readonly s3Service: S3Service) {}
+  constructor(
+    private readonly s3Service: S3Service,
+    private readonly config: ConfigService,
+  ) {}
 
   async uploadFiles(id: string, files: Array<Express.Multer.File>) {
     return {
+      url: this.config.get<string>("S3_PUBLIC_URL"),
       paths: await Promise.all(
         files.map(
           async (file) =>
